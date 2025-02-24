fb08afec5894d90d0e8aa5f082d92517a06cf26d
Simranpreet <154065727+Simranpreet10@users.noreply.github.com>
2025-02-24 09:23:57 +0530

    Add files via upload

--- userRoute.js
+++ userRoute.js
@@ -1,6 +1,8 @@
 const  express = require("express");
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
+const { TokenExpiredError } = require("jsonwebtoken");
+const { sendmail } = require("../utils/sendmail");
 const Prisma = new PrismaClient()
 
 router.get("/:email",async(req,res)=>{
@@ -12,14 +14,26 @@ router.get("/:email",async(req,res)=>{
     })
     res.json({user});
 })
-router.get("/:email",async(req,res)=>{
-    const {email,name} = req.body;
+router.post("/",async(req,res)=>{
+    const {email,name,password} = req.body;
     let newUser = await Prisma.user.create({
         data:{
             email:email,
-            name:name
+            name:name,
+            password:password
+        }
+    });
+    let token = Math.floor(Math.random()*10000);
+
+    let newtoken = await Prisma.token.create({
+        data:{
+            token:token,
+            userid:newUser.id
         }
     })
+
+    let link = `http://localhost:4245/verify/${token}/${newUser.id}`
+     await sendmail(email,"verify email",link)
     res.json({newUser})
 })
 
